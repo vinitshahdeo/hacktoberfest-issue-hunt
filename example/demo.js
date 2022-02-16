b49837e332f51a9a29e3c17d3e3aa6e1409fc242
whddnjs1715 <whddnjs1715@gmail.com>
2022-02-17 08:33:09 +0900

    first-commit without token

--- example/demo.js
+++ example/demo.js
@@ -1,6 +1,6 @@
 const hacktoberfest = require("../lib/index"),
   options = {
-    token: "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", // replace it with your token
+    token: "XXXXXXXXXXXXXXX", // replace it with your token
     labels: "first-timers-only",
     limit: 10,
     language: "js",
